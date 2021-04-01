c30508bc55955c32bd9b989fe9baca414feb044c
vinay0458 <vinay0458@gmail.com>
2021-04-01 14:42:24 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="grid-container">
+      <header>
+        <a href="/">React Shopping Cart</a>
+      </header>
+      <main>Product List</main>
+      <footer>all rights are reserved</footer>
+      
+    </div>
+  );
+}
+
+export default App;
